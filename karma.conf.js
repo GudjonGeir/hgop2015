79b9e4c56c164adb9ada80065eec60ee0f7cec80
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2015-12-02 15:59:13 +0000

    Added karma-junit-reporter

--- karma.conf.js
+++ karma.conf.js
@@ -57,6 +57,16 @@ module.exports = function(config) {
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
     logLevel: config.LOG_INFO,
 
+	reporters: ['progress', 'junit'],
+
+    // the default configuration
+    junitReporter: {
+        outputDir: 'junitResults', // results will be saved as $outputDir/$browserName.xml
+        outputFile: 'karma-junit-results.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+        suite: 'karma', // suite will become the package name attribute in xml testsuite element
+        useBrowserName: true // add browser name to report and classes names
+    },
+
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
