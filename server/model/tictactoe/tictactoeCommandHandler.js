a004298dec17db78ff8fa83211faddae6a2ca61a
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2015-12-03 14:09:24 +0000

    ticTacToe command handler: user can join an existing game

--- server/model/tictactoe/tictactoeCommandHandler.js
+++ server/model/tictactoe/tictactoeCommandHandler.js
@@ -1,14 +1,24 @@
 module.exports = function tictactoeCommandHandler(events) {
 	return {
 		executeCommand: function executeCommand(command) {
+			if (command.cmd === "CreateGame") {
+				return [
+					{
+						event     : 'GameCreated',
+						gameName  : command.gameName,
+						userName  : command.userName,
+						timeStamp : command.timeStamp
+					}
+				];
+			}
 			return [
 				{
-					event     : 'GameCreated',
-					gameName  : command.gameName,
-					userName  : command.userName,
-					timeStamp : command.timeStamp
+					event: 'GameJoined',
+					gameName: command.gameName,
+					userName: command.userName,
+					timeStamp: command.timeStamp
 				}
 			];
 		}
-	}
+	};
 };
--- server/model/tictactoe/whenCreateGame.spec.js
+++ server/model/tictactoe/whenCreateGame.spec.js
@@ -34,6 +34,7 @@ describe('create game command', () => {
 		given = [
 			{
 				event     : 'GameCreated',
+				gameName  : 'game1',
 				userName  : 'User1',
 				timeStamp : '2015.01.01T10:00:00'
 			}
@@ -56,5 +57,34 @@ describe('create game command', () => {
 		var actual = tictactoeCommandHandler(given).executeCommand(when);
 
 		JSON.stringify(actual).should.be.exactly(JSON.stringify(then));
+	});
+});
+
+describe('join game command', () => {
+	var given, when, then;
+
+	it('should allow a user to join an existing game', () => {
+		given = [
+			{
+				event     : 'GameCreated',
+				gameName  : 'game1',
+				userName  : 'User1',
+				timeStamp : '2015.01.01T10:00:00'
+			}
+		];
+		when = {
+			cmd       : 'JoinGame',
+			gameName  : 'game1',
+			userName  : 'User2',
+			timeStamp : '2015.01.01T10:01:00'
+		};
+		then = [
+			{
+				event: 'GameJoined',
+				gameName: 'game1',
+				userName: 'User2',
+				timeStamp: '2015.01.01T10:01:00'
+			}
+		];
 	});
 });
