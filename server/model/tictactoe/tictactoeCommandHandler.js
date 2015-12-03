552fa0ec6f754177498df9a3e344eb0e51aa5f8c
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2015-12-03 13:30:57 +0000

    ticTacToe command handler: user can create a game

--- server/model/tictactoe/tictactoeCommandHandler.js
+++ server/model/tictactoe/tictactoeCommandHandler.js
@@ -1,3 +1,13 @@
 module.exports = function tictactoeCommandHandler(events) {
-
+	return {
+		executeCommand: function executeCommand(command) {
+			return [
+				{
+					event     : 'GameCreated',
+					userName  : 'User1',
+					timeStamp : '2015.01.01T10:00:00'
+				}
+			];
+		}
+	}
 };
--- server/model/tictactoe/whenCreateGame.spec.js
+++ server/model/tictactoe/whenCreateGame.spec.js
@@ -1,1 +1,26 @@
 var tictactoeCommandHandler = require('./tictactoeCommandHandler');
+
+describe('create game command', () => {
+	var given, when, then;
+
+	it('should create game', () =>{
+		given = [];
+		when = {
+			cmd       : 'CreateGame',
+			userName  : 'User1',
+			gameName  : 'game1',
+			timeStamp : '2015.01.01T10:00:00'
+		};
+		then = [
+			{
+				event     : 'GameCreated',
+				userName  : 'User1',
+				timeStamp : '2015.01.01T10:00:00'
+			}
+		];
+
+		var actual = tictactoeCommandHandler(given).executeCommand(when);
+
+		JSON.stringify(actual).should.be.exactly(JSON.stringify(then));
+	});
+});
