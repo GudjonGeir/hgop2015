fd4b86c15ab4e18328cf2996077e02e45aa1b02c
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2015-12-08 14:56:55 +0000

    Fixed acceptance url issue with acceptance test script

--- acceptance-test.sh
+++ acceptance-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ -z "$1" ]; then echo "Empty parameter for testenv IP Address"; exit -1; fi
 npm install
-export ACCEPTANCE_URL=http:"$1"
+export ACCEPTANCE_URL=http://"$1"
 grunt mochaTest:acceptance
