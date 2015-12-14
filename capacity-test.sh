a20a4ddc8ba2caef292829ea9db0f8eff0c55b12
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2015-12-14 14:41:30 +0000

    Added testenv port to deploy, acceptance and capacity scripts. Added  to deploy script

--- capacity-test.sh
+++ capacity-test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 if [ -z "$1" ]; then echo "Empty parameter for testenv IP Address"; exit -1; fi
-export MOCHA_REPORTER=xunit
-export MOCHA_REPORT=server-tests.xml
+if [ -z "$2" ]; then echo "Empty parameter for testenv PORT"; exit -1; fi
 npm install
-export ACCEPTANCE_URL=http://"$1":9000
+export ACCEPTANCE_URL=http://"$1":"$2"
 grunt mochaTest:load
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 if [ -z "$1" ]; then echo "Empty parameter for testenv IP Address"; exit -1; fi
+if [ -z "$2" ]; then echo "Empty parameter for testenv PORT"; exit -1; fi
+if [ -z "$3" ]; then echo "Empty parameter for last successful commit"; exit -1; fi
 
 echo Executing remote...
 ssh vagrant@"$1" "docker stop tictactoe;
-							docker rm tictactoe;
-							docker pull ggjonsson/tictactoe;
-							docker run -p 9000:8080 --name tictactoe -d -e "NODE_ENV=production" ggjonsson/tictactoe"
+				docker rm tictactoe;
+				docker pull ggjonsson/tictactoe"$2";
+				docker run -p 9000:"$2" --name tictactoe -d -e "NODE_ENV=production" ggjonsson/tictactoe"$2""
