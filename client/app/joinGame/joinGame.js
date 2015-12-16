4c77d23ba108671c476a51f9b2e5612d4ca2b1c4
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2015-12-16 23:51:21 +0000

    Changed spaces to tabs

--- client/app/joinGame/joinGame.js
+++ client/app/joinGame/joinGame.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('tictactoeApp')
-  .config(function ($stateProvider) {
-    $stateProvider
-      .state('join', {
-        url: '/join/{gameId}',
-        templateUrl: 'app/joinGame/joinGame.html',
-        controller: 'JoinGameCtrl'
-      });
-  });
+	.config(function ($stateProvider) {
+		$stateProvider
+			.state('join', {
+				url: '/join/{gameId}',
+				templateUrl: 'app/joinGame/joinGame.html',
+				controller: 'JoinGameCtrl'
+			});
+	});
--- client/app/tictactoeController/tictactoeController.controller.js
+++ client/app/tictactoeController/tictactoeController.controller.js
@@ -1,62 +1,62 @@
 'use strict';
 
 angular.module('tictactoeApp')
-  .controller('TictactoeController', function ($scope, $http, gameState, guid, $location, $interval) {
+	.controller('TictactoeController', function ($scope, $http, gameState, guid, $location, $interval) {
 
-    $scope.gameState = gameState();
+		$scope.gameState = gameState();
 
-    var thenHandleEvents = function (postPromise) {
-      postPromise.then(function (data) {
-        $scope.gameState.mutate(data.data);
-      });
+		var thenHandleEvents = function (postPromise) {
+			postPromise.then(function (data) {
+				$scope.gameState.mutate(data.data);
+			});
 
-      postPromise.then(function(){
+			postPromise.then(function(){
 
-        if (mySide() === 'X'){
-          $scope.me = $scope.gameState.creatingUser;
-          $scope.other = $scope.gameState.joiningUser;
-        } else {
-          $scope.other = $scope.gameState.creatingUser;
-          $scope.me = $scope.gameState.joiningUser;
-        }
+				if (mySide() === 'X'){
+					$scope.me = $scope.gameState.creatingUser;
+					$scope.other = $scope.gameState.joiningUser;
+				} else {
+					$scope.other = $scope.gameState.creatingUser;
+					$scope.me = $scope.gameState.joiningUser;
+				}
 
-        $scope.joinUrl = 'http://' + $location.host() +( $location.port() ? ':' + $location.port() :'') + '/join/' + $scope.gameState.gameId;
+				$scope.joinUrl = 'http://' + $location.host() +( $location.port() ? ':' + $location.port() :'') + '/join/' + $scope.gameState.gameId;
 
-      });
-    };
+			});
+		};
 
 
-    var gameId = $location.search().gameId;
+		var gameId = $location.search().gameId;
 
-    function refresh() {
-      thenHandleEvents($http.get('/api/gameHistory/' + gameId));
-    }
+		function refresh() {
+			thenHandleEvents($http.get('/api/gameHistory/' + gameId));
+		}
 
-    refresh();
-    $interval(refresh, 2000);
+		refresh();
+		$interval(refresh, 2000);
 
-    function mySide() {
-      return $location.search().gameSide;
-    }
+		function mySide() {
+			return $location.search().gameSide;
+		}
 
-    $scope.myTurn = function () {
-      return mySide() === $scope.gameState.nextTurn;
-    };
+		$scope.myTurn = function () {
+			return mySide() === $scope.gameState.nextTurn;
+		};
 
-    $scope.placeMove = function (coords) {
-      if(!$scope.myTurn()){
-        return;
-      }
-      thenHandleEvents($http.post('/api/placeMove/', {
-          gameId: $scope.gameState.gameId,
-          comm: 'PlaceMove',
-          user: $scope.me,
-          timeStamp: '2014-12-02T11:29:29',
-          move: {
-            xy: coords,
-            side: mySide()
-          }
-        }
-      ));
-    };
-  });
+		$scope.placeMove = function (coords) {
+			if(!$scope.myTurn()){
+				return;
+			}
+			thenHandleEvents($http.post('/api/placeMove/', {
+					gameId: $scope.gameState.gameId,
+					comm: 'PlaceMove',
+					user: $scope.me,
+					timeStamp: '2014-12-02T11:29:29',
+					move: {
+						xy: coords,
+						side: mySide()
+					}
+				}
+			));
+		};
+	});
