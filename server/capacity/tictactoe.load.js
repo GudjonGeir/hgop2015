57942d143aae37126138812fd14f092d9c3719ca
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2015-12-17 14:20:31 +0000

    Changed load test to 10 seconds

--- server/capacity/tictactoe.load.js
+++ server/capacity/tictactoe.load.js
@@ -1,10 +1,10 @@
 var user = require('../fluid-api/tictactoeFluid').user;
 var given = require('../fluid-api/tictactoeFluid').given;
 
-it('Should play 1000 games in x seconds.', function (done) {
+it('Should play 300 games in x seconds.', function (done) {
 	var doneCount = 0;
-	var gamesToPlay = 1000;
-	var x = 20;
+	var gamesToPlay = 300;
+	var x = 10;
 
 	this.timeout(x * 1000);
 
