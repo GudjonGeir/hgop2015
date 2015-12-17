9c2a5d610f21ba3487880d8df2a8854c60f08c25
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2015-12-17 14:09:14 +0000

    Added port to docker image name for deploy script to differentiate between test and prod

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ if [ -z "$2" ]; then echo "Empty parameter for PORT"; exit -1; fi
 if [ -z "$3" ]; then echo "Empty parameter for last successful commit"; exit -1; fi
 
 echo Executing remote...
-ssh vagrant@"$1" "docker stop tictactoe;
-				docker rm tictactoe;
+ssh vagrant@"$1" "docker stop tictactoe-"$2";
+				docker rm tictactoe-"$2";
 				docker pull ggjonsson/tictactoe:"$3";
-				docker run -p "$2":8080 --name tictactoe -d -e "NODE_ENV=production" ggjonsson/tictactoe:"$3""
+				docker run -p "$2":8080 --name tictactoe-"$2" -d -e "NODE_ENV=production" ggjonsson/tictactoe:"$3""
